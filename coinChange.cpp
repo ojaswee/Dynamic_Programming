19cf7a0dc20cec9071f984649bd02cdc02388b81
ojaswee <ojaswee@outlook.com>
2017-12-22 11:20:28 -0600

    Redefining matrix

--- coinChange.cpp
+++ coinChange.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
+#include <algorithm> // for minuimum function
 
 using namespace std; 
 
-int numberOfCoins = 3;
+const int numberOfCoins = 3;
+const int target = 11; 
+int p[numberOfCoins][target];
 
 //this is bottom up approach
-void coinChange(int change, int avilableCoins[]) {
-	int **p = { 0 };
-	for (int remaining = 0; remaining <= change; remaining++) {
-		cout << "\n";
-		for (int coins = 0; coins < numberOfCoins; coins++) {
-			if (change == avilableCoins[coins]) {
-				p[coins][remaining] = 1;
-				cout << p[coins][remaining] << "\t";
+void coinChange( int avilableCoins[]) {
+	//int **p = { 0 };
+	//int **p = new int[numberOfCoins*change];
+	for (int remaining = 1; remaining <= numberOfCoins; remaining++) {
+		for (int coins = 1; coins <= target; coins++) {
+			if (target == avilableCoins[coins-1]) {
+				p[remaining][coins] = 1;
+				//cout << p[remaining][coins] << "\t";
 			}
-			else if (remaining == 0) {
-				p[coins][remaining] = 0;
-				cout << p[coins][remaining] << "\t";
+			else if (coins > avilableCoins[remaining-1] && avilableCoins[remaining-1] == 1) {
+				p[remaining][coins] = 1+ p[remaining][coins-avilableCoins[remaining-1]];
+				//cout << p[remaining][coins] << "\t";
 			}
-			else if (remaining > 0 && (change != avilableCoins[coins])) {
-				p[coins][remaining] = 1 + p[coins - 1][remaining];
-				cout << p[coins][remaining] << "\t";
+			else if (coins > avilableCoins[remaining-1] && avilableCoins[remaining-1] != 1) {
+				p[remaining][coins] =min(p[remaining-1][coins],1+p[remaining][coins-1]); //error here 
+				//cout << p[remaining][coins] << "\t";
 			}
+			else if (coins<avilableCoins[remaining-1]) {
+				p[remaining][coins] = p[remaining - 1][coins];
+				//cout << p[remaining][coins] << "\t";
+			}
+			cout << p[remaining][coins] << "\t";
 		}
+		cout << "\n";
 	}
 }
 
 int main() {
-	int coins[] = {1,5,10};
-	coinChange(11, coins);
+	int coins[] = { 1,5,10 };
+	coinChange(coins);
 	system("pause");
 }
