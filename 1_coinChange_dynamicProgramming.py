b6fa3d3391f265eff4661c1e02dc5f830fb4d963
ojaswee <ojaswee@outlook.com>
2017-12-23 11:08:30 -0600

    coinChange in Python

---
+++ 1_coinChange_dynamicProgramming.py
@@ -0,0 +1,19 @@
+#this is bottom-up approach
+
+def findnumofchange(change,n):
+    mat = [[0 for x in range(n+1)] for y in range(len(change)+1)]
+    for c in range(1,len(change)+1): # available coins
+        for m in range(1,n+1):  # change needed
+            if c ==1:
+                mat[c][m] = mat[c][m-1]+1
+            elif (m >= change[c-1]):
+                mat[c][m] = min(1+ mat[c][m-change[c-1]],mat[c-1][m])
+            else:
+                mat[c][m] = mat[c-1][m]
+    return mat
+
+mat = (findnumofchange([1,6,10],12))
+for i in mat:
+    print i
+
+
